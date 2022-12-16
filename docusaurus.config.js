af5db2b46ebbb33904767f2a004305f3d43e9230
hannes <hannesdelbeke@gmail.com>
2022-12-16 12:33:45 +0000

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,55 +1,55 @@
 const path = require("path");
 
 module.exports = {
-  title: "THE DATA ENGINEERING COOKBOOK",
-  tagline: "by ANDREAS KRETZ",
-  url: "http://cookbook.learndataengineering.com",
-  baseUrl: "/", // path where website is available
+  title: "DEV NOTES",
+  tagline: "by HANNES D",
+  url: "https://hannesdelbeke.github.io/",
+  baseUrl: "/notes", // path where website is available
   favicon: "images/CookbookCover.jpg",
-  organizationName: "andkret", // GitHub org/user name.
-  projectName: "Cookbook", // project name i.e repo name of project
+  organizationName: "Hannes", // GitHub org/user name.
+  projectName: "Notes", // project name i.e repo name of project
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   themeConfig: {
     colorMode: {
-      defaultMode: "light",
-      disableSwitch: true,
+      defaultMode: "dark",
+      disableSwitch: false,
       respectPrefersColorScheme: true,
     },
-    navbar: {
-      title: "Data Engineering Cookbook",
-      logo: {
-        alt: "Site Logo",
-        src: "images/CookbookCover.jpg", // path with respect to static dir
-      },
-      items: [
-        {
-          to: "docs/01-Introduction",
-          label: "Cookbook",
-          position: "left",
-        },
-        {
-          href: "https://learndataengineering.com/",
-          label: "Data Engineering Academy",
-          position: "right",
-        },
-        {
-          href: "https://medium.com/plumbersofdatascience",
-          label: "Plumbers Of Data Science",
-          position: "right",
-        },
-        {
-          href: "https://github.com/andkret/Cookbook",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "light",
-      links: [],
-      copyright: `Copyright © ${new Date().getFullYear()} Andreas Kretz. Built by Kristijan Bakaric with Docusaurus.`,
-    },
+    // navbar: {
+    //   title: "Data Engineering Cookbook",
+    //   logo: {
+    //     alt: "Site Logo",
+    //     src: "images/CookbookCover.jpg", // path with respect to static dir
+    //   },
+    //   items: [
+    //     {
+    //       to: "docs/01-Introduction",
+    //       label: "Cookbook",
+    //       position: "left",
+    //     },
+    //     {
+    //       href: "https://learndataengineering.com/",
+    //       label: "Data Engineering Academy",
+    //       position: "right",
+    //     },
+    //     {
+    //       href: "https://medium.com/plumbersofdatascience",
+    //       label: "Plumbers Of Data Science",
+    //       position: "right",
+    //     },
+    //     {
+    //       href: "https://github.com/andkret/Cookbook",
+    //       label: "GitHub",
+    //       position: "right",
+    //     },
+    //   ],
+    // },
+    // footer: {
+    //   style: "light",
+    //   links: [],
+    //   copyright: `Copyright © ${new Date().getFullYear()} Andreas Kretz. Built by Kristijan Bakaric with Docusaurus.`,
+    // },
     // prism: {
     //   additionalLanguages: ["powerquery", "dax"],
     // },
